d3ecfc6ed63dd8ba9dc4b2efcae779e858638eb0
Anish Tiwari <anish.at.tiwari@gmail.com>
2024-08-07 23:52:43 +0530

    Feat: store api keys in env

--- src/app/config.ts
+++ src/app/config.ts
@@ -18,7 +18,7 @@ export const config = createConfig(
     {
         // if you don't want to leak api keys, you can proxy to a backend and set the rpcUrl instead here
         // get this from the app config you create at https://dashboard.alchemy.com/accounts
-        apiKey: 'lBgt-MGMRswmlE3aCCHF__-5HnBcUlxi',
+        apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || '',
         chain: sepolia,
         ssr: true, // set to false if you're not using server-side rendering
         storage: cookieStorage,
