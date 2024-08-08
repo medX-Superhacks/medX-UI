7bd14595b5808c5b0a6db0b03baf23ebba8ba007
Anish Tiwari <anish.at.tiwari@gmail.com>
2024-08-09 01:24:12 +0530

    Chore: Build fixes

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,7 +2,7 @@
 const HomePage = () => {
     return (
         <div className="py-10 flex items-center justify-between">
-            Something is cooking, we're on it
+            Something is cooking, we are on it
         </div>
     );
 };
