0000ed885cd8e0d2158df0f30216a9efe063c496
Vib-UX <vibchess123@gmail.com>
2024-08-10 00:05:53 +0530

    adds eas createPrescription alchemy

--- src/app/lib/func.tsx
+++ src/app/lib/func.tsx
@@ -73,7 +73,64 @@ export const createMedicalRecordAttestation = async (
     };
 
     const transaction = await eas.attest(attestationData);
+    const multiProof = privateData.generateMultiProof([0,1,2,3]);
+    console.log('Multi-proof for selective reveal (Medical Record):', multiProof);
+    
     return {
         data: transaction.data,
+        zkProofMedicalRecord: multiProof
     };
 };
+
+export const createPrescriptionAttestation = async (
+    prescriptionId: string,
+    medication: string,
+    dosage: string,
+    duration: string,
+    recipient: string,
+    signer: any,
+    refUID: string = ethers.constants.HashZero // Default to HashZero if not provided
+  ) => {
+    const eas = new EAS(easContractAddress);
+    eas.connect(signer);
+  
+    // Create private data for the prescription
+    const privateData = new PrivateData([
+      { type: 'string', name: 'prescriptionId', value: prescriptionId },
+      { type: 'string', name: 'medication', value: medication },
+      { type: 'string', name: 'dosage', value: dosage },
+      { type: 'string', name: 'duration', value: duration }
+    ]);
+  
+    // Get the full tree for the prescription
+    const fullTreePrescription = privateData.getFullTree();
+  
+    // Encode the prescription data using the SchemaEncoder
+    const schemaEncoderPrescription = new SchemaEncoder('bytes32 privateData');
+    const encodedDataPrescription = schemaEncoderPrescription.encodeData([
+      { name: 'privateData', value: fullTreePrescription.root, type: 'bytes32' }
+    ]);
+  
+    // Prepare the attestation data
+    const attestationData: any = {
+      schema: schemaUID,
+      data: {
+        recipient,
+        expirationTime: 0,
+        revocable: true,
+        refUID,
+        data: encodedDataPrescription,
+      }
+    };
+  
+    const transaction = await eas.attest(attestationData);
+    const multiProof = privateData.generateMultiProof([0, 1, 2, 3]);
+    console.log('Multi-proof for selective reveal (Prescription):', multiProof);
+  
+    return {
+      data: transaction.data,
+      zkProofPrescription: multiProof
+    };
+  };
+  
+
